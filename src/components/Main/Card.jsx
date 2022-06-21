3f543928ec0d8a6cd930e4fe83b4b1522f1bbb40
raw20 <raw1250@gmail.com>
2022-06-21 19:08:58 +0900

    cardList 추가

--- src/components/Main/Card.jsx
+++ src/components/Main/Card.jsx
@@ -1,8 +1,29 @@
 import React from "react";
-import styled from "styled-components";
+import { useEffect } from "react";
+import { useState } from "react";
+import CardItem from "./CardItem";
 
-function Card(props) {
-  return <div>CARD LIST</div>;
+function Card() {
+  const [cardData, setCardData] = useState([]);
+  const dataURL = "./db/cardData.json";
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(dataURL);
+      const json = await response.json();
+      setCardData(json);
+    })();
+  });
+  return (
+    <div className="card-area">
+      <h2>cardList</h2>
+      <ul className="card-wrap">
+        {cardData.map((card) => (
+          <CardItem key={card.id} card={card} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Card;
---
+++ src/components/Main/CardItem.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { MdFavorite } from "react-icons/md";
+import { useState } from "react";
+
+function CardItem({ card }) {
+  const favListColor = [{ color: "#ddd" }, { color: "#f20" }];
+  const btnColor = [
+    { color: "#fff", fontWeight: 700, backgroundColor: "#ada" },
+    { color: "#fff", fontWeight: 700, backgroundColor: "rgb(26, 183, 52)" },
+  ];
+  const [like, setLike] = useState(0);
+  const [checkState, setCheckState] = useState("");
+  function handlerLikeFnc(event) {
+    setLike(like === 0 ? 1 : 0);
+    setCheckState(event.target.checked);
+  }
+  return (
+    <li>
+      <div
+        className="img_set"
+        style={{ background: `url(${card.imgUrl}) no-repeat center / cover` }}
+      ></div>
+      <dl>
+        <dt>{card.title}</dt>
+        <dd>{card.content}</dd>
+      </dl>
+      <div className="btns">
+        <input
+          type="checkbox"
+          id={card.checkName}
+          name={card.checkName}
+          data-checked={checkState}
+          onChange={handlerLikeFnc}
+          className="blind"
+        />
+        <label htmlFor={card.checkName} style={btnColor[like]}>
+          좋아요
+          <MdFavorite style={favListColor[like]} />
+        </label>
+      </div>
+    </li>
+  );
+}
+
+export default CardItem;
